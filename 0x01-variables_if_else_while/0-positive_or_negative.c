7445fcc61a7d48aa69f74a6186402556f8374fc8
M0111Ange <angemukundente@gmail.com>
2023-07-14 04:00:39 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,31 @@
-#!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number < 0:
-    sign = "is negative"
-elif number > 0:
-    sign = "is positive"
-else:
-    sign = "is zero"
-print("{:d}".format(number), sign)
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Brennan D Baraban
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+
+	return (0);
+}
