d983f9c910d82c7b8c60f3dcb9e16204ed400060
M0111Ange <angemukundente@gmail.com>
2023-07-14 08:52:45 -0700

    6

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,24 @@
-#!/usr/bin/python3
-# 100-print_tebahpla.py
-# Brennan D Baraban <375@holbertonschool.com>
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: Brennan D Baraban
+ */
 
-""""Print the alphabet in reverse order alternating upper- and lower-case."""
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
