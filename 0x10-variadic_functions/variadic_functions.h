5abec0ba120e548ab74583d4d099abb6042e1d52
M0111Ange <angemukundente@gmail.com>
2023-08-17 03:35:33 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,9 @@
-#include <stdarg.h>
-#ifndef VARIADIC_FUNCTIONS
-#define VARIADIC_FUNCTIONS
-
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int print_case(const char sample, va_list ap);
-int still_match(const char *rem);
 
-#endif /* VARIADIC_FUNCTIONS */
+#endif
